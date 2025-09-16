837d405eadfb42161aeddb0778f2a5d998f10cf3
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-09-16 12:09:40 +0200

    this is the testing

--- client/src/lib/axios-client.ts
+++ client/src/lib/axios-client.ts
@@ -11,16 +11,18 @@ const options = {
 
 const API = axios.create(options);
 
+API.interceptors.request.use()
+
 API.interceptors.response.use(
   (response) => {
     return response;
   },
   async (error) => {
-    const { data, status } = error.response;
+    const { data } = error.response;
 
-    if (data === "Unauthorized" && status === 401) {
-      window.location.href = "/";
-    }
+    // if (data === "Unauthorized" && status === 401) {
+    //   window.location.href = "/";
+    // }
 
     const customError: CustomError = {
       ...error,
