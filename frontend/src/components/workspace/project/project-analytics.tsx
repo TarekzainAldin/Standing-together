bf8462ddfb52523fc59f2c396516c5a004d09cdf
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-10-18 11:33:36 +0200

    finsh translate and analytics projects

--- frontend/src/components/workspace/project/project-analytics.tsx
+++ frontend/src/components/workspace/project/project-analytics.tsx
@@ -3,11 +3,12 @@ import AnalyticsCard from "../common/analytics-card";
 import useWorkspaceId from "@/hooks/use-workspace-id";
 import { useQuery } from "@tanstack/react-query";
 import { getProjectAnalyticsQueryFn } from "@/lib/api";
+import { useTranslation } from "react-i18next";
 
 const ProjectAnalytics = () => {
+  const { t } = useTranslation(); // i18n hook
   const param = useParams();
   const projectId = param.projectId as string;
-
   const workspaceId = useWorkspaceId();
 
   const { data, isPending } = useQuery({
@@ -23,17 +24,17 @@ const ProjectAnalytics = () => {
     <div className="grid gap-4 md:gap-5 lg:grid-cols-2 xl:grid-cols-3">
       <AnalyticsCard
         isLoading={isPending}
-        title="Total Task"
+        title={t("projectAnalytics.totalTasks")}
         value={analytics?.totalTasks || 0}
       />
       <AnalyticsCard
         isLoading={isPending}
-        title="Overdue Task"
+        title={t("projectAnalytics.overdueTasks")}
         value={analytics?.overdueTasks || 0}
       />
       <AnalyticsCard
         isLoading={isPending}
-        title="Completed Task"
+        title={t("projectAnalytics.completedTasks")}
         value={analytics?.completedTasks || 0}
       />
     </div>
