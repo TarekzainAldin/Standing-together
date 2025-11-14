486dd32a973e7e2b1228062ec1ec97547431bc1a
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-11-14 11:01:49 +0100

    trying to deleting the file for dest

---
+++ backend/dist/utils/jwt.js
@@ -0,0 +1,28 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.signJwtToken = exports.accessTokenSignOptions = void 0;
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const app_config_1 = require("../config/app.config");
+const defaults = {
+    audience: ["user"],
+};
+// const expiresInSeconds: number = parseInt(getEnv("JWT_EXPIRES_IN_SECONDS", "86400"));
+// export const accessTokenSignOptions: SignOptsAndSecret = {
+//   expiresIn: expiresInSeconds,
+//   secret: getEnv("JWT_SECRET"),
+// };
+exports.accessTokenSignOptions = {
+    expiresIn: app_config_1.config.JWT_EXPIRES_IN,
+    secret: app_config_1.config.JWT_SECRET,
+};
+const signJwtToken = (payload, options) => {
+    const { secret, ...opts } = options || exports.accessTokenSignOptions;
+    return jsonwebtoken_1.default.sign(payload, secret, {
+        ...defaults,
+        ...opts,
+    });
+};
+exports.signJwtToken = signJwtToken;
