c0eee9c860a6aa7cdb3397d49e54bb4be9b3bc5b
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-11-16 13:20:17 +0100

    make jest for all the test folder and passing 49 test

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,11 +1,8 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
+/** @type {import('jest').Config} */
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    ...tsJestTransformCfg,
-  },
+  preset: 'ts-jest',           // يستخدم ts-jest
+  testEnvironment: 'node',     // بيئة Node.js
+  roots: ['<rootDir>/src'],    // يشمل ملفات src فقط
+  testMatch: ['**/tests/**/*.test.ts'], // يشمل ملفات الاختبار
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
 };
