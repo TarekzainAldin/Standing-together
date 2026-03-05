28e2b48353200c379c8c23df568c6f700e7c41a6
TAREKZAINALDIN <tarekzindin@gmail.com>
2026-03-05 21:00:56 +0100

    change the port

--- backend/src/config/app.config.ts
+++ backend/src/config/app.config.ts
@@ -3,7 +3,7 @@ import { getEnv } from "../utils/get-env";
 
 const appConfig = () => ({
     NODE_ENV: getEnv("NODE_ENV", "development"),
-    PORT: getEnv("PORT", "5000"),
+    PORT: getEnv("PORT", "5173"),
     BASE_PATH: getEnv("BASE_PATH", "/api"),
     MONGO_URI: getEnv("MONGO_URI", ""),
 
